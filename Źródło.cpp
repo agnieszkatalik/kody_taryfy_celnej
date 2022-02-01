263c67ad96651c1d2f50427ef23b504d9cb6bc74
agnieszkatalik <talik@student.agh.edu.pl>
2022-02-01 12:31:17 +0100

    zautomatyzowane wrzucanie przedmiotów do tablicy

--- Źródło.cpp
+++ Źródło.cpp
@@ -5,10 +5,9 @@
 
 using namespace std;
 
-#define n 13567 // iloœæ linii pliku
-#define cc 41 // liczba rzeczy
 
-void function(int *custom_code, int *qnt, string *item_name, string *country_of_origin, double *value_eur, string *name)
+
+void function(int *custom_code, int *qnt, string *item_name, string *country_of_origin, double *value_eur, string *items, int n, int m)
 {
 	for (int i = 0; i < n - 1; i++)
 	{
@@ -39,17 +38,17 @@ void function(int *custom_code, int *qnt, string *item_name, string *country_of_
 						value_eur[i] = 0;
 
 						custom_code[i] = 0;
-						for (int l = 0; l < cc; l++)
+						for (int l = 0; l < m; l++)
 						{
-							size_t found_j = item_name[j].find(name[l]);
-							size_t found_i = item_name[i].find(name[l]);
+							size_t found_j = item_name[j].find(items[l]);
+							size_t found_i = item_name[i].find(items[l]);
 							if (found_i != string::npos && found_j != string::npos)
 							{
 							}
 
 							else if (found_i != string::npos)
 							{
-								item_name[j] += ", " + name[l];
+								item_name[j] += ", " + items[l];
 
 							}
 
@@ -64,18 +63,39 @@ void function(int *custom_code, int *qnt, string *item_name, string *country_of_
 	}
 }
 
+string* names(string *item_name, int n, int m) {
+
+	string* tmp = new string[100];
+	bool t = false;
+
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = 0; j < m + 1; j++)
+		{
+			if (tmp[j] == item_name[i])
+				t = true;
+
+		}
+		if (t == false)
+		{
+			tmp[m] = item_name[i];
+			m++;
+		}
+		t = false;
+	}
+	string* names = new string[m];
+	for (int i = 0; i < m; i++)
+	{
+		names[i] = tmp[i];
+	}
+	delete[] tmp;
+	return names;
+}
+
 int main()
 {
-
-	int* custom_code = new int[n];
-	int* qnt = new int[n];
-	string* item_name = new string[n];
-	string* country_of_origin = new string[n];
-	double* value_eur = new double[n];
-	double price_unit_euro;
-	string name[cc] = { "0","A","ACCESSORIES","BABY BIB","BABY SUIT","BAG","BATHROBE","BEACHWEAR","BELT","BLAZER","BODY","CAP","CARDIGAN","COAT","DRESS","JACKET","KIDS ACCESSORIES","LEGGINGS","MAJICA","NIGHTWEAR","OVERAL","PANTS","RIANCOAT","RAINCOAT","SCARF","SET","SHIRT","SHOES","SKIRT","SOCKS","SORTS","SUIT","SWEATER","SWEATSHIRT","TIE","TOP","TRACKSUIT","T-SHIRT","UNDERWEAR","VEST","WALLET"};
-
-	int i = 0;
+	static int n = 0;
+	static int m = 0; // to bêdzie liczba przedmiotów
 
 	ifstream file;
 	file.open("kody.txt", std::ios::in | std::ios::out);
@@ -86,42 +106,63 @@ int main()
 	}
 	else
 	{
+			string line;
+			while (getline(file, line))
+			{
+				n++;
+			}
+
+			file.close();
+
+			int* custom_code = new int[n];
+			int* qnt = new int[n];
+			string* item_name = new string[n];
+			string* country_of_origin = new string[n];
+			double* value_eur = new double[n];
+			double price_unit_euro;
+
+			file.open("kody.txt", std::ios::in | std::ios::out);
 
 		for(int i=0; i<n; i++)
 		{
 			file >> item_name[i] >> custom_code[i] >> country_of_origin[i] >> qnt[i] >> price_unit_euro >> value_eur[i];
 		}
+
 		price_unit_euro = 0;
 
 		file.close();
 
-	}
 
-	function(custom_code, qnt, item_name, country_of_origin, value_eur, name);
 
-	ofstream done("gotowe.txt");
-	for (int i = 0; i < n; i++)
-	{
-		//cout << i << ": " << item_name[i] << " " << custom_code[i] << " " << country_of_origin[i] << " " << qnt[i] << " " << value_eur[i] << endl;
-		done << item_name[i] << "\t" << custom_code[i] << "\t" << country_of_origin[i] << "\t" << qnt[i] << "\t" << value_eur[i] << endl;
-	}
-	
-	delete [] custom_code, qnt, item_name, country_of_origin, value_eur, name;
+		string* items = names(item_name, n, m);
+		function(custom_code, qnt, item_name, country_of_origin, value_eur, items, n, m);
 
-	done.close();
 
-	fstream done_tmp;
-	done_tmp.open("gotowe.txt", ios::in | ios::out);
 
-	if (done_tmp.good() == false)
-	{
-		cout << "File failed to open" << endl;
-		exit(0);
-	}
+		ofstream done("gotowe.txt");
+		for (int i = 0; i < n; i++)
+		{
+			//cout << i << ": " << item_name[i] << " " << custom_code[i] << " " << country_of_origin[i] << " " << qnt[i] << " " << value_eur[i] << endl;
+			done << item_name[i] << "\t" << custom_code[i] << "\t" << country_of_origin[i] << "\t" << qnt[i] << "\t" << value_eur[i] << endl;
+		}
 
-	else
-	{
-		ofstream done("kody_gotowe.txt");
+		
+		delete[] custom_code, qnt, item_name, country_of_origin, value_eur, items;
+
+		done.close();
+
+		fstream done_tmp;
+		done_tmp.open("gotowe.txt", ios::in | ios::out);
+
+		if (done_tmp.good() == false)
+		{
+			cout << "File failed to open" << endl;
+			exit(0);
+		}
+
+		else
+		{
+			ofstream done("kody_gotowe.txt");
 
 			string linia;
 			do
@@ -139,8 +180,10 @@ int main()
 			done_tmp.close();
 
 			remove("gotowe.txt");
+		}
+
 	}
-	
-	return 0;
+
+		return 0;
 
 }
